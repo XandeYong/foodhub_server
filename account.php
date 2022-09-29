311516a1fab6bf964a9cdc75629a5876c245b65e
darrenkaw29 <kawkz-wm19@student.tarc.edu.my>
2022-09-29 17:45:31 +0800

    update account

--- account.php
+++ account.php
@@ -1,3 +1,4 @@
+
 <?php 
 
 if (isset($_POST['request'])) {
@@ -15,35 +16,30 @@ if (isset($_POST['request'])) {
         $password = $_POST['password'];
     
         $sql = "SELECT * FROM account where email = '$email' && password = '$password' LIMIT 1";
-    
-        if (!$conn->query($sql)) {
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) <= 0) {
             $message = "no data";
             $status = "1";
         } else {
-    
-            $account = array();
-            while($row = mysqli_fetch_assoc($result)) {
-                
-                $image = $row['image'] . ".jpg";
-                $row['image'] = "http://10.0.2.2/foodhub_server/image/" . "account/" . $image;
-    
-                $account[] = $row;
+            
+            $analysis_report = array();
+            while ($row = mysqli_fetch_assoc($result)) {
+                $analysis_report[] = $row;
             }
 
-            $message = "account retrived success";
+            $message = "data retrived success";
             $status = "0";
-    
+
             $json_body = array(
-                "data" => $account,
+                "data" => $analysis_report,
                 "message" => $message,
                 "status" => $status
             );
     
             echo $json = json_encode($json_body);
         }
-    
-        
-    } else if ($request == "register" 
+        }else if ($request == "register" 
     && isset($_POST['id']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['accountType']) ) {
     
         $id = $_POST['id'];
@@ -54,36 +50,76 @@ if (isset($_POST['request'])) {
     
         //check account
         $sql = "SELECT * FROM account where email = '$email'";
-    
-        if (!$conn->query($sql)) {
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) == 0) {
             
             //register
-            $sql = "insert into account(account_id, name, email, password, account_type) values('$id', '$name', '$email', '$password', '$account_type')";
-        
-            if (!$conn->query($sql)) {
+            $sql = "insert into account(account_id, name, email, password, account_type) values('$id', '$name', '$email', '$password', '$type')";
+            $result = mysqli_query($conn, $sql);
+
+            if (!$result) {
                 $message = "Fail to register.";
                 $status = "-2";
             } else {
                 $message = "Register successfully";
                 $status = "0";
             }
-
-        } else {
+        }
+        else {
             $message = "This email has already been registered.";
             $status = "1";
         }
 
         $json_body = array(
             "message" => $message,
-            "status" => $status
+            "status" => $status,
         );
 
         echo $json = json_encode($json_body);
+
+    }
+    }else if (isset($_GET['request']) && isset($_GET['accountType'])){  
+        $request = $_GET['request'];  
+        $type = $_GET['accountType'];
+
+        if($request == "registerGetId"){
+      
+
+        $sql = "SELECT account_id FROM account where account_type = '$type' Order By updated_at Asc";
+
+        require_once "conn.php";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) <= 0) {
+            $message = "no data";
+            $status = "1";
+        } else {
+            
+           $getID;
+            while ($row = mysqli_fetch_assoc($result)) {
+               $getID = $row;
+            }
+
+            $message = "data retrived success";
+            $status = "0";
+
+            $json_body = array(
+                "data" => $getID,
+                "message" => $message,
+                "status" => $status
+            );
+    
+            echo $json = json_encode($json_body);
+        } 
+    }
+
     }
 
 
+
     $conn->close();
-}
+
 
 
 
