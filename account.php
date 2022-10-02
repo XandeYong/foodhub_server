e0722a960c5f6ebb50ffd684bac24b42a87e612a
Adrian Fong <adrain2000@live.com>
2022-10-02 12:44:10 +0800

    #

--- account.php
+++ account.php
@@ -1,65 +1,67 @@
-<?php 
+<?php
 
 if (isset($_POST['request'])) {
     $request = $_POST['request'];
-    
+
     $message = "error";
     $status = "-1";
-    
+
     require_once "conn.php";
 
-    if ($request == "login" 
-    && isset($_POST['email']) && isset($_POST['password'])) {
-    
+    if (
+        $request == "login"
+        && isset($_POST['email']) && isset($_POST['password'])
+    ) {
+
         $email = $_POST['email'];
         $password = $_POST['password'];
-    
+
         $sql = "SELECT * FROM account where email = '$email' && password = '$password' LIMIT 1";
-    
+
         if (!$conn->query($sql)) {
             $message = "no data";
             $status = "1";
         } else {
-    
+
             $account = array();
-            while($row = mysqli_fetch_assoc($result)) {
-                
+            while ($row = mysqli_fetch_assoc($result)) {
+
                 $image = $row['image'] . ".jpg";
                 $row['image'] = "http://10.0.2.2/foodhub_server/image/" . "account/" . $image;
-    
+
                 $account[] = $row;
             }
 
             $message = "account retrived success";
             $status = "0";
-    
+
             $json_body = array(
                 "data" => $account,
                 "message" => $message,
                 "status" => $status
             );
-    
+
             echo $json = json_encode($json_body);
         }
-    
-        
-    } else if ($request == "register" 
-    && isset($_POST['id']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['accountType']) ) {
-    
+    } else if (
+        $request == "register"
+        && isset($_POST['id']) && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['accountType'])
+    ) {
+
         $id = $_POST['id'];
         $name = $_POST['name'];
         $email = $_POST['email'];
         $password = $_POST['password'];
         $type = $_POST['accountType'];
-    
+
         //check account
         $sql = "SELECT * FROM account where email = '$email'";
-    
+
         if (!$conn->query($sql)) {
-            
+
             //register
             $sql = "insert into account(account_id, name, email, password, account_type) values('$id', '$name', '$email', '$password', '$account_type')";
-        
+
             if (!$conn->query($sql)) {
                 $message = "Fail to register.";
                 $status = "-2";
@@ -67,7 +69,6 @@ if (isset($_POST['request'])) {
                 $message = "Register successfully";
                 $status = "0";
             }
-
         } else {
             $message = "This email has already been registered.";
             $status = "1";
@@ -79,13 +80,44 @@ if (isset($_POST['request'])) {
         );
 
         echo $json = json_encode($json_body);
+    } else if (
+        $request == "UpdateAccount"
+        && isset($_POST['accountID']) && isset($_POST['accountName'])  && isset($_POST['accountImage'])  && isset($_POST['accountAddress'])  && isset($_POST['accountState'])
+        && isset($_POST['accountDOB']) && isset($_POST['accountGender']) && isset($_POST['accountEmail']) && isset($_POST['accountPassword']) && isset($_POST['accountType'])
+    ) {
+
+        //Account Variables
+        $accountID = $_POST['accountID'];
+        $accountName = $_POST['accountName'];
+        $accountImage = $_POST['accountImage'];
+        $accountAddress = $_POST['accountAddress'];
+        $accountState = $_POST['accountState'];
+        $accountDOB = $_POST['accountDOB'];
+        $accountGender = $_POST['accountGender'];
+        $accountEmail = $_POST['accountEmail'];
+        $accountPassword = $_POST['accountPassword'];
+        $accountType = $_POST['accountType'];
+
+        //Update Status
+        $sql = "UPDATE account SET name = '$accountName', image = '$accountImage', address = '$accountAddress', state_id = '$accountState', bod = '$accountDOB',
+        gender = '$accountGender', email = '$accountEmail', password = '$accountPassword', account_type = '$accountType' WHERE account_id = '$accountID'";
+
+
+        if (!$conn->query($sql)) {
+            $message = "Fail to update into Remote DB";
+            $status = "-2";
+        } else {
+            $message = "Succesfully update into Remote DB successfully";
+            $status = "0";
+        }
+        $json_body = array(
+            "message" => $message,
+            "status" => $status
+        );
+
+        echo $json = json_encode($json_body);
     }
 
 
     $conn->close();
 }
-
-
-
-
-?>
--- category.php
+++ category.php
@@ -2,10 +2,10 @@
 
 if (isset($_GET['request'])) {
     $request = $_GET['request'];
-    
+
     $message = "error";
     $status = "-1";
-    
+
     require_once "conn.php";
 
     if ($request == 'getAll') {
@@ -17,7 +17,7 @@ if (isset($_GET['request'])) {
             $message = "no data";
             $status = "1";
         } else {
-            
+
             $category = array();
             while ($row = mysqli_fetch_assoc($result)) {
                 $category[] = $row;
@@ -31,13 +31,92 @@ if (isset($_GET['request'])) {
                 "message" => $message,
                 "status" => $status
             );
-    
+
             echo $json = json_encode($json_body);
         }
     }
+    $conn->close();
+} else if (isset($_POST['request'])) {
+    $request = $_POST['request'];
 
+    $message = "error";
+    $status = "-1";
+
+    require_once "conn.php";
+
+    if (
+        $request == "UpdateCategory"
+        && isset($_POST['categoryID']) && isset($_POST['categoryName'])
+    ) {
+
+        $categoryID = $_POST['categoryID'];
+        $categoryName = $_POST['categoryName'];
+
+        //Update Status
+        $sql = "UPDATE category SET name = '$categoryName' WHERE category_id = '$categoryID'";
+
+        if (!$conn->query($sql)) {
+            $message = "Fail to update into Remote DB";
+            $status = "-2";
+        } else {
+            $message = "Successfully update into Remote DB";
+            $status = "0";
+        }
+        $json_body = array(
+            "message" => $message,
+            "status" => $status
+        );
+
+        echo $json = json_encode($json_body);
+    } else if (
+        $request == "AddCategory"
+        && isset($_POST['categoryID']) && isset($_POST['categoryName'])
+    ) {
+
+        $categoryID = $_POST['categoryID'];
+        $categoryName = $_POST['categoryName'];
+
+        //Add Status
+        $sql = "INSERT INTO category(category_id, name) values('$categoryID', '$categoryName')";
+
+
+        if (!$conn->query($sql)) {
+            $message = "Fail to insert into Remote DB";
+            $status = "-2";
+        } else {
+            $message = "Successfully insert into Remote DB";
+            $status = "0";
+        }
+        $json_body = array(
+            "message" => $message,
+            "status" => $status
+        );
+
+        echo $json = json_encode($json_body);
+    } else if (
+        $request == "DeleteCategory"
+        && isset($_POST['categoryID'])
+    ) {
+
+        $categoryID = $_POST['categoryID'];
+
+        //Delete Status
+        $sql = "DELETE FROM category WHERE category_id = '$categoryID'";
+
+
+        if (!$conn->query($sql)) {
+            $message = "Fail to delete from Remote DB";
+            $status = "-2";
+        } else {
+            $message = "Successfully deleted from Remote DB";
+            $status = "0";
+        }
+        $json_body = array(
+            "message" => $message,
+            "status" => $status
+        );
+
+        echo $json = json_encode($json_body);
+    }
     $conn->close();
 }
-
-
-?>
